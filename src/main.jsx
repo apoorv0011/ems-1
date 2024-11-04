45390bae5058fb36f6130135e674d9e49a209481
Apoorv <apoorv.pachori0803@gmail.com>
2024-11-05 00:21:34 +0530

    1st commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import AuthProvider from './context/AuthProvider.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import AuthProvider from './context/AuthProvider.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
 
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
 
-  </React.StrictMode>,
-)
+//   </React.StrictMode>,
+// )
